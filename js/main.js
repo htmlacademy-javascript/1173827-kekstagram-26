3cfde14bcb6097ba9c0620a953aff82c067fc6a8
Александр <sanya32melnik@mail.ru>
2022-06-18 18:49:14 +0300

    задание 4.15 исправление 2

--- js/main.js
+++ js/main.js
@@ -19,14 +19,14 @@ const PHOTO_DESCRIPTIONS =[
   'Описание9',
 ];
 
-const textForComment = `Всё отлично!
-  В целом всё неплохо. Но не всё.
-  Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.
-  Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
-  Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
-  Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`;
-
-const ARRAY_SENTENCES = textForComment.split('\n');
+const TEXT_FOR_COMMENT = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
 //Функция, возвращающая случайное целое число
 function getRandomPositiveInteger(a, b) {
@@ -35,12 +35,6 @@ function getRandomPositiveInteger(a, b) {
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 }
-
-/*
-//Функция для проверки максимальной длины строки
-const checkStringLength = (testString, maxLength) => testString.length <= maxLength;
-//checkStringLength();
-*/
 
 //Получение рандомного элемента из переданного массива
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
@@ -60,25 +54,27 @@ function getNumberWithoutRepeats(min,max){
 
 const genDescriptionId  = getNumberWithoutRepeats(1,25);
 const genImageAddressId = getNumberWithoutRepeats(1,25);
-const genCommentId = getNumberWithoutRepeats(1,1000);
+const genCommentId = getNumberWithoutRepeats(1,999);
 
 //Функция создания комментария
 const createComment = ()=> ({
   id:genCommentId(),
   avatar:`img/avatar-${getRandomPositiveInteger(1,6)}.svg`,
-  message:getRandomArrayElement(ARRAY_SENTENCES).trim(),
+  message:getRandomArrayElement(TEXT_FOR_COMMENT),
   name:getRandomArrayElement(NAMES)
 });
 
+const getSetComments = () => Array.from({length:2},createComment);
+
 // Функция создания объекта c информацией о фото
-const createObject = () => ({
+const createDescriptionPhoto = () => ({
   id:genDescriptionId(),
   url:`photos/${genImageAddressId()}.jpg`,
   description:getRandomArrayElement(PHOTO_DESCRIPTIONS),
   likes:getRandomPositiveInteger(15,200),
-  comments:createComment(),
+  comments:getSetComments(),
 });
 
 //Получение массива объектов
 // eslint-disable-next-line no-unused-vars
-const arrayObject = Array.from({length:25}, createObject);
+const arrayObject = Array.from({length:25}, createDescriptionPhoto);
